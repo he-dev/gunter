76f8ff2d62fc4a074f211733f9c06e0b7d25b4a9
x-t3ch <bennyboss+github@gmail.com>
2017-06-20 14:41:13 +0200

    - bug fixes

---
+++ Gunter/TehCodez/Extensions/DataRowExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Gunter.Extensions
+{
+    internal static class DataRowExtensions
+    {
+        public static IEnumerable<object> Select(this IEnumerable<DataRow> dataRows, string columnName)
+        {
+            return dataRows.Select(dataRow => dataRow[columnName]);
+        }
+
+        // ReSharper disable once InconsistentNaming
+        public static IEnumerable<object> NotDBNull(this IEnumerable<object> dataRows)
+        {
+            return dataRows.Where(value => value != DBNull.Value);
+        }        
+    }
+}
--- Gunter/TehCodez/Reporting/Modules/DataSummary.cs
+++ Gunter/TehCodez/Reporting/Modules/DataSummary.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.ComponentModel;
 using System.Data;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using Gunter.Data;
+using Gunter.Extensions;
+using Gunter.Reporting.Data;
 using Gunter.Reporting.Filters;
 using JetBrains.Annotations;
-using Newtonsoft.Json;
 using Reusable.Data;
 
 namespace Gunter.Reporting.Modules
@@ -19,17 +18,17 @@ namespace Gunter.Reporting.Modules
     [PublicAPI]
     public class DataSummary : Module, ITabular
     {
-        private delegate object AggregateCallback(IEnumerable<DataRow> aggregate, string columnName);
+        private delegate object AggregateCallback([NotNull, ItemCanBeNull] IEnumerable<object> values);
 
         private static readonly Dictionary<ColumnTotal, AggregateCallback> Aggregates = new Dictionary<ColumnTotal, AggregateCallback>
         {
-            [ColumnTotal.First] = (rows, column) => rows.Select(column).NotDBNull().FirstOrDefault(),
-            [ColumnTotal.Last] = (rows, column) => rows.Select(column).NotDBNull().LastOrDefault(),
-            [ColumnTotal.Min] = (rows, column) => rows.Select(column).NotDBNull().Select(Convert.ToDouble).AggregateOrDefault(Enumerable.Min, double.NaN),
-            [ColumnTotal.Max] = (rows, column) => rows.Select(column).NotDBNull().Select(Convert.ToDouble).AggregateOrDefault(Enumerable.Max, double.NaN),
-            [ColumnTotal.Count] = (rows, column) => rows.Select(column).NotDBNull().Count(),
-            [ColumnTotal.Sum] = (rows, column) => rows.Select(column).NotDBNull().Select(Convert.ToDouble).Sum(),
-            [ColumnTotal.Average] = (rows, column) => rows.Select(column).NotDBNull().Select(Convert.ToDouble).AggregateOrDefault(Enumerable.Average, double.NaN),
+            [ColumnTotal.First] = values => values.FirstOrDefault(),
+            [ColumnTotal.Last] = values => values.LastOrDefault(),
+            [ColumnTotal.Min] = values => values.Select(Convert.ToDouble).AggregateOrDefault(Enumerable.Min, double.NaN),
+            [ColumnTotal.Max] = values => values.Select(Convert.ToDouble).AggregateOrDefault(Enumerable.Max, double.NaN),
+            [ColumnTotal.Count] = values => values.Count(),
+            [ColumnTotal.Sum] = values => values.Select(Convert.ToDouble).Sum(),
+            [ColumnTotal.Average] = values => values.Select(Convert.ToDouble).AggregateOrDefault(Enumerable.Average, double.NaN),
         };
 
         public TableOrientation Orientation => TableOrientation.Horizontal;
@@ -80,133 +79,45 @@ namespace Gunter.Reporting.Modules
             }
 
             // Create aggregated rows and add them to the final data-table.
-            var rows = rowGroups.Select(rowGroup => AggregateRows(dataTable, columns, rowGroup));
+            var rows = rowGroups.Select(rowGroup => AggregateRows(dataTable, columns, rowGroup.ToList()));
             foreach (var row in rows)
             {
                 dataTable.Rows.Add(row);
             }
 
             // Add the footer row with column options.
-            IEnumerable<string> GetColumnOptions(ColumnOption column)
+            IEnumerable<string> StringifyColumnOption(ColumnOption column)
             {
                 if (column.IsKey) yield return "Key";
                 if (column.Filter != null && !(column.Filter is Unchanged)) yield return column.Filter.GetType().Name;
                 yield return column.Total.ToString();
             }
-            dataTable.AddRow(columns.Select(column => (object)string.Join(", ", GetColumnOptions(column))).ToArray());
+            dataTable.AddRow(columns.Select(column => (object)string.Join(", ", StringifyColumnOption(column))).ToArray());
 
             return dataTable;
         }
 
-        private DataRow AggregateRows(DataTable dataTable, IEnumerable<ColumnOption> columns, IGrouping<CompositeKey<object>, DataRow> group)
+        private DataRow AggregateRows(DataTable dataTable, IEnumerable<ColumnOption> columns, ICollection<DataRow> rowGroup)
         {
             var dataRow = dataTable.NewRow();
 
             foreach (var column in columns.Where(c => !c.Equals(ColumnOption.GroupCount)))
             {
-                // Try to get an aggregate function.
                 var aggregate = Aggregates[column.Total];
-
-                var value = aggregate(group, column);
+                var values = rowGroup.Select(column).NotDBNull();
+                var value = aggregate(values);
                 dataRow[column.Name] = column.Filter == null ? value : column.Filter.Apply(value);
             }
 
             if (IncludeGroupCount)
             {
-                dataRow[ColumnOption.GroupCount] = group.Count();
+                dataRow[ColumnOption.GroupCount] = rowGroup.Count();
             }
             return dataRow;
         }
     }
 
     // Represents an aggregated column and its options.
-    [DebuggerDisplay("{" + nameof(DebuggerDisplay) + ",nq}"), PublicAPI]
-    public class ColumnOption : IEquatable<ColumnOption>
-    {
-        public static readonly ColumnOption GroupCount = new ColumnOption
-        {
-            Name = "GroupCount",
-            Total = ColumnTotal.Count
-        };
-
-        [JsonRequired]
-        public string Name { get; set; }
-
-        public bool IsKey { get; set; }
-
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public IDataFilter Filter { get; set; } = new Unchanged();
-
-        public ColumnTotal Total { get; set; }
-
-        private string DebuggerDisplay => $"Name = {Name} IsKey = {IsKey} Filter = {Filter?.GetType().Name ?? "null"} Total = {Total}";
-
-        public bool Equals(ColumnOption other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return StringComparer.OrdinalIgnoreCase.Equals(Name, other.Name);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((ColumnOption)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return StringComparer.OrdinalIgnoreCase.GetHashCode(Name);
-        }
-
-        public static implicit operator string(ColumnOption column) => column.Name;
-    }
-
-    public enum ColumnTotal
-    {
-        First,
-        Last,
-        Min,
-        Max,
-        Count,
-        Sum,
-        Average,
-    }
-
-    public class CompositeKey<T> : IEnumerable<T>, IEquatable<CompositeKey<T>>
-    {
-        private readonly List<T> _keys;
-
-        public CompositeKey(IEnumerable<T> keys) => _keys = new List<T>(keys);
-
-        public IEnumerator<T> GetEnumerator() => _keys.GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        public bool Equals(CompositeKey<T> other)
-        {
-            if (ReferenceEquals(other, null)) return false;
-            return ReferenceEquals(this, other) || this.SequenceEqual(other);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((CompositeKey<T>)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return this.Aggregate(0, (current, next) => (current * 397) ^ next?.GetHashCode() ?? 0);
-            }
-        }
-    }
 
     //public class CollectionComparer<TValue> : IEqualityComparer<IEnumerable<TValue>>
     //{
@@ -225,35 +136,4 @@ namespace Gunter.Reporting.Modules
     //        }
     //    }
     //}
-
-    internal static class DataRowExtensions
-    {
-        public static IEnumerable<object> Select(this IEnumerable<DataRow> dataRows, string columnName)
-        {
-            return dataRows.Select(dataRow => dataRow[columnName]);
-        }
-
-        // ReSharper disable once InconsistentNaming
-        public static IEnumerable<object> NotDBNull(this IEnumerable<object> dataRows)
-        {
-            return dataRows.Where(value => value != DBNull.Value);
-        }        
-    }
-
-    internal static class DoubleExtensions
-    {
-        /// <summary>
-        /// Aggregates throw if the collection is empty. Make sure it isn't before calculating.
-        /// </summary>
-        /// <param name="values"></param>
-        /// <param name="aggregate"></param>
-        /// <param name="defaultValue"></param>
-        /// <returns></returns>
-        public static double AggregateOrDefault(this IEnumerable<double> values, Func<IEnumerable<double>, double> aggregate, double defaultValue)
-        {
-            // Min throws if the collection is empty.
-            //values = values.ToList();
-            return values.Any() ? aggregate(values) : defaultValue;
-        }
-    }
 }
